4b9791a371b84aa315054398c4402f6d88740688
Craig Martin <cmart1220@gmail.com>
2015-06-15 20:39:51 -0400

    Initial Commit

---
+++ BitDate_MadkittyTests/BitDate_MadkittyTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitDate_MadkittyTests.swift
+//  BitDate_MadkittyTests
+//
+//  Created by Craig Martin on 6/15/15.
+//  Copyright (c) 2015 MadKitty. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BitDate_MadkittyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
